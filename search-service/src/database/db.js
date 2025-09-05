6134152f447323df672d5d490f7864f506d3f609
harsh6029sharma <harshsharmahs.081@gmail.com>
2025-09-05 19:59:27 +0530

    db file change

--- search-service/src/database/db.js
+++ search-service/src/database/db.js
@@ -1,11 +1,26 @@
-const mongoose = require('mongoose')
-const logger = require('../utils/logger')
+// const mongoose = require('mongoose')
+// const logger = require('../utils/logger')
 
-const connectToDB = async() =>{
-    await mongoose.connect(process.env.MONGODB_URI)
-    .then(()=> logger.info('Connected to mongodb successfully!'))
-    .catch((e)=>logger.error('Mongodb connection error!',e))
+// const connectToDB = async() =>{
+//     await mongoose.connect(process.env.MONGODB_URI)
+//     .then(()=> logger.info('Connected to mongodb successfully!'))
+//     .catch((e)=>logger.error('Mongodb connection error!',e))
     
-}
+// }
 
-module.exports = connectToDB
+// module.exports = connectToDB
+const connectWithRetry = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000,
+    });
+    logger.info('MongoDB connected successfully!');
+  } catch (err) {
+    logger.error('MongoDB connection failed, retrying in 5s...', err);
+    setTimeout(connectWithRetry, 5000);
+  }
+};
+
+connectWithRetry();
